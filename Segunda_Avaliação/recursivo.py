9be44c0756577e7b9fd9b9531e342b279e5b9e4a
SamylleRose <samylle1brito@gmail.com>
2026-05-28 16:55:25 -0300

    fi: comment

--- Segunda_Avaliação/recursivo.py
+++ Segunda_Avaliação/recursivo.py
@@ -1,13 +1,11 @@
-# recursivo.py
 
 def multiplicacao_matrizes_recursiva(p, i, j):
-    # Caso base: apenas uma matriz, custo de multiplicação é 0
+    # Caso base
     if i == j:
         return 0
     
     min_custo = float('inf')
     
-    # Testa todas as posições possíveis para "quebrar" a cadeia
     for k in range(i, j):
         custo = (multiplicacao_matrizes_recursiva(p, i, k) +
                  multiplicacao_matrizes_recursiva(p, k + 1, j) +
